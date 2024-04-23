f4da4b4dc7a1b64352cec43d9ba488551643d934
codeniaaz <muktarabdiaziz12@gmail.com>
2024-04-23 03:06:18 +0300

    export_to_CSV

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-""" Gather data from an API """
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
 
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id)
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    user_data = user_response.json()
-    todos_data = todos_response.json()
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for task in todos_data if task['completed'])
-    completed_tasks_titles = [task['title']
-                              for task in todos_data if task['completed']]
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-    print("Employee {} is done with tasks{}/{}):".format(
-        user_data['name'], completed_tasks, total_tasks))
-    for task_title in completed_tasks_titles:
-        print("\t{}".format(task_title))
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
